7f263ae3b6ba5bdba19c70092f4d222e3a30cc13
TonyKun <tal96047@gmail.com>
2024-12-13 12:42:19 +0200

    Template Nodejs

--- Services/webServices.js
+++ Services/webServices.js
@@ -7,37 +7,53 @@ router.get('/', (req, res) => {
 //get all users 
   router.get('/test-get', (req, res) => {
     console.log(users);
-    res.send(users);
+    res.status(200).send(users);
   })
 
 //create a new user and id
 router.post('/test-post', (req, res) => {
     const {name,id}=req.body 
+    const haveID =users.find(user => user.id===id)
+    if(haveID){
+        console.log('User already exists');
+        return res.status(400).send('User already exists');
+    }
     const newUser = { id, name };
     users.push(newUser);
-
     console.log(users);
-    res.send(users);
+     res.status(200).send(users);
   })
   //update a user by id
   router.put('/test-put/:id', (req, res) => {
     const { name } = req.body;
     const { id } = req.params;
+    let haveID =false;
     users = users.map(user => {
         if (user.id == id) {
+          haveID = true;
             return { ...user, name };
         }
         return user;
     });
+    if(!haveID){
+        console.log('User not found');
+        return res.status(404).send('User not found');
+    }
     console.log(users);
-    res.send(users);
+    res.status(200).send(users);
 });
 
 //delete user by id
 router.delete('/test-delete/:id', (req, res) => {
     const { id } = req.params;
-    users = users.filter(user => user.id!= id);
+    const user = users.findIndex(user => user.id === id);
+
+    if (user === -1) {
+        console.log('User not found');
+        return res.status(404).send('User not found');
+    }
+    users.splice(user, 1);
     console.log(users); 
-    res.send(users);
+    res.status(200).send(users);
 })
 module.exports = router;
